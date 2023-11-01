800acea70e044b0e415b89e2b7956a554d4ee96a
solomonkrow <metro317@live.com>
2023-11-01 11:49:15 -0500

    established post comment reply routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,127 @@
+const router = require('express').Router();
+const { User, Post, Comment, Reply } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
+        const posts = postData.map((post) => post.get({ plain: true}));
+
+        res.render('homepage', {
+            posts
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const comments = commentData.map((comment) => comment.get({ plain: true}));
+
+        res.render('homepage', {
+            comments
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const replyData = await Reply.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const replies = replyData.map((reply) => reply.get({ plain: true}));
+
+        res.render('homepage', {
+            replies
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
+
+        const posts = postData.get({ plain: true });
+
+        res.render('posts', {
+            ...posts,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('comments', async (req, res) => {
+    try {
+        const commentData = await Comment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
+
+        const comments = commentData.get({ plain: true });
+
+        res.render('', {
+            ...comments,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('replies', async (req, res) => {
+    try {
+        const replyData = await Reply.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
+
+        const replies = replyData.get({ plain: true });
+
+        res.render('', {
+            ...replies,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
