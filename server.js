78bc7c781446ecc7ca28aa702d9ccf876d05bef5
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-11-02 12:05:56 -0500

    historical events api added to controllers and server.js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-// const axios = require('axios');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -39,6 +39,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+app.get('/historical-events', routeHandler.getHistoricalEvents);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
