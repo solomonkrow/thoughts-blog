0510631a4ac67d5ab73d13522acb9dad814f69c7
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-04 15:41:12 -0500

    prep fpr merge

--- controllers/api/historyRoutes.js
+++ controllers/api/historyRoutes.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
+// const express = require('express');
+// const router = express.Router();
+// const axios = require('axios');
 
-// Route handler for fetching data from the Numbers API based on the current date
-router.get('/numbersapi', async (req, res) => {
-    try {
-        // Get the current date and month
-        const currentDate = new Date();
-        const month = currentDate.getMonth() + 1; // Note: January is 0-based index, so add 1 to get the actual month
-        const day = currentDate.getDate();
+// // Route handler for fetching data from the Numbers API based on the current date
+// router.get('/numbersapi', async (req, res) => {
+//     try {
+//         // Get the current date and month
+//         const currentDate = new Date();
+//         const month = currentDate.getMonth() + 1; // Note: January is 0-based index, so add 1 to get the actual month
+//         const day = currentDate.getDate();
 
-        // Construct the API URL with the current month and day
-        const apiUrl = `http://numbersapi.com/${month}/${day}/date`;
+//         // Construct the API URL with the current month and day
+//         const apiUrl = `http://numbersapi.com/${month}/${day}/date`;
 
-        // Make a request to the constructed API endpoint
-        const response = await axios.get(apiUrl);
+//         // Make a request to the constructed API endpoint
+//         const response = await axios.get(apiUrl);
 
-        // Send the API response back to the client
-        res.status(200).send(response.data);
-    } catch (error) {
-        // Handle errors and send an error response to the client
-        console.error('Error fetching data from Numbers API:', error);
-        res.status(500).send('Error fetching data from the Numbers API');
-    }
-});
+//         // Send the API response back to the client
+//         res.status(200).send(response.data);
+//     } catch (error) {
+//         // Handle errors and send an error response to the client
+//         console.error('Error fetching data from Numbers API:', error);
+//         res.status(500).send('Error fetching data from the Numbers API');
+//     }
+// });
 
-module.exports = router;
+// module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,12 @@ const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 const replyRoutes = require('./replyRoutes');
-const historyRoutes = require('./historyRoutes');
+// const historyRoutes = require('./historyRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 router.use('/replies', replyRoutes);
-router.use('/history', historyRoutes);
+// router.use('/history', historyRoutes);
 
 module.exports = router;
