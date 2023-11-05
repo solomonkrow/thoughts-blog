23ec9dce931b8a674fdcb4d0833c14637f439835
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-05 15:00:54 -0600

    updated seed data for presentation prep

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,24 +2,29 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        title: 'First Post',
-        content: 'This is the content of the first post.',
+        title: 'History is cool!',
+        content: 'Glad to have found this website so I can talk to my fellow history buffs!',
         user_id: 1
     },
     {
-        title: 'Second Post',
-        content: 'This is the content of the second post.',
+        title: 'Awesome!',
+        content: 'This place is cool.',
         user_id: 2
     },
     {
-        title: 'Third Post',
-        content: 'This is the content of the third post.',
+        title: 'I love Ancient Roman history',
+        content: 'Lets discuss it here!',
         user_id: 3
     },
     {
-        title: 'Fourth Post',
-        content: 'This is the content of the fourth post.',
+        title: 'History is kinda boring',
+        content: 'The past is irrelevant, everyone who is cool knows the future is where its at.',
         user_id: 4
+    },
+    {
+        title: 'This is a delete-able Post',
+        content: 'Delete Me!',
+        user_id: 1
     }
 ];
 
--- seeds/reply-seeds.js
+++ seeds/reply-seeds.js
@@ -2,24 +2,29 @@ const { Reply } = require('../models');
 
 const replyData = [
     {
-        content: 'This is a reply to the first comment.',
+        content: 'Totally!',
         user_id: 3,
         comment_id: 1
     },
     {
-        content: 'This is a reply to the second comment.',
+        content: 'Welcome!',
         user_id: 4,
         comment_id: 2
     },
     {
-        content: 'This is another reply to the second comment.',
-        user_id: 2,
-        comment_id: 2
+        content: 'Praetorians assemble!',
+        user_id: 1,
+        comment_id: 3
     },
     {
-        content: 'This is a reply to the fourth comment.',
-        user_id: 1,
+        content: 'This place sucks. Later nerds.',
+        user_id: 4,
         comment_id: 4
+    },
+    {
+        content: 'I will also be destroyed when my post is deleted! DESTROY!',
+        user_id: 1,
+        comment_id: 3
     }
 ];
 
